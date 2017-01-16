ec32de04c1c05fec8e24381dfa0ba3c44f4dbc24
pranav <pranav@pranav-PC>
2017-01-15 22:54:13 -0500

    checking the commit

--- src/main/java/com/example/EurekaServiceApplication.java
+++ src/main/java/com/example/EurekaServiceApplication.java
@@ -3,11 +3,16 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
 
+	
+	
 	public static void main(String[] args) {
+		System.out.println("starting the application");
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 }
